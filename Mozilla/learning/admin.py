b1f625a97d0b70bb729f38826096099cb28f9741
gerryfrank10 <gerryfrank720@gmail.com>
2020-08-30 17:22:03 +0300

    The Models, Admin, And views implementation

--- Mozilla/learning/admin.py
+++ Mozilla/learning/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Book, BookInstance, Author, Genre
 
 # Register your models here.
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name','date_of_birth')
+    fields = ['first_name','last_name',('date_of_birth','date_of_death')]
+admin.site.register(Author, AuthorAdmin)
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
--- Mozilla/learning/views.py
+++ Mozilla/learning/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import BookInstance, Book, Author, Genre
+
 
 # Create your views here.
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all()
+    
+
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances' : num_instances,
+        'num_authors' : num_authors,
+    }
+    return render(request, 'index.html',context=context)
